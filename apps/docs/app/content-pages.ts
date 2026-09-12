a0024383590677852c6fb31aa12eab4edbbe6db6
Lloyd Richards <lloyd.d.richards@gmail.com>
2026-09-12 19:04:21 +0200

    docs: add initial learning pages

---
+++ apps/docs/app/content-pages.ts
@@ -0,0 +1,23 @@
+export const contentPages = [
+  {
+    section: "Start",
+    label: "Getting Started",
+    href: "/getting-started",
+    routePath: "getting-started",
+    contentPath: "content/getting-started.mdx"
+  },
+  {
+    section: "Concepts",
+    label: "Which Package Should I Use?",
+    href: "/concepts/which-package",
+    routePath: "concepts/which-package",
+    contentPath: "content/concepts/which-package.mdx"
+  },
+  {
+    section: "Guides",
+    label: "Testing with an Isolated Filesystem",
+    href: "/guides/testing-with-an-isolated-filesystem",
+    routePath: "guides/testing-with-an-isolated-filesystem",
+    contentPath: "content/guides/testing-with-an-isolated-filesystem.mdx"
+  }
+] as const
--- apps/docs/app/routes.ts
+++ apps/docs/app/routes.ts
@@ -1,8 +1,10 @@
 import { index, prefix, route, type RouteConfig } from "@react-router/dev/routes"
 import { apiPages } from "./api-pages"
+import { contentPages } from "./content-pages"
 
 const routes = [
   index("content/index.mdx"),
+  ...contentPages.map(({ routePath, contentPath }) => route(routePath, contentPath)),
   ...prefix(
     "api",
     apiPages.map(({ routePath, contentPath }) => route(routePath, contentPath))
