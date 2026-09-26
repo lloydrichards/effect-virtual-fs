1e95db3905a06ed378a3aa1e10580e3904cbc9e2
Lloyd Richards <lloyd.d.richards@gmail.com>
2026-09-26 19:15:53 +0200

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "effect-virtual-fs development environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = system: import nixpkgs { inherit system; };
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              bun
+              nodejs_24
+              git
+            ];
+            shellHook = ''
+              echo "Bun $(bun --version)"
+              echo "Node $(node --version)"
+            '';
+          };
+        }
+      );
+    };
+}
